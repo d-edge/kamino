6d46d44779d8b68447b1e42885a01e089f64f5d9
adegouvello <adegouvello@d-edge.com>
2021-05-19 14:25:34 +0200

    feat(print): add a print only mode

--- kamino/Program.fs
+++ kamino/Program.fs
@@ -2,16 +2,22 @@ open System
 open Argu
 open Argu.ArguAttributes
 open LibGit2Sharp
+open System.Text.Json.Serialization
 
 // # doc: https://docs.gitlab.com/ee/api/groups.html#list-a-groups-s-subgroups
 
 type Project =
-    { path_with_namespace: string
-      http_url_to_repo: string }
+    { [<JsonPropertyName("path_with_namespace")>]
+      Path: string
+      [<JsonPropertyName("http_url_to_repo")>]
+      Url: string }
 
-let clone url path =
-    Repository.Clone(url, path) |> ignore
-    printfn "%s" path
+let clone url path printMode =
+    if printMode then
+        printfn "%s" path
+    else
+        Repository.Clone(url, path) |> ignore
+        printfn "%s" path
 
 let inline (</>) path1 path2 = IO.Path.Combine(path1, path2)
 
@@ -22,20 +28,25 @@ let download token (url: string) =
 
 let deserialize (json: string) =
     Text.Json.JsonSerializer.Deserialize<seq<Project>> json
-
+// ?private_token={token} ???
 let inline buildUrl url group =
-    $"https://{url}/api/v4/groups/{group}/projects?include_subgroups=true&per_page=100&page=1"
+    $"https://{url}/api/v4/groups/{group}/projects?include_subgroups=true&simple=true&per_page=100&page=1"
 
 let insertToken baseAddress token (url: string) =
     url.Replace(baseAddress, $"oauth2:{token}@{baseAddress}")
 
-let cloneOrganisation baseAddress group path token =
+let cloneOrganisation baseAddress group path token printMode =
     let clone project =
-        let url = insertToken baseAddress token project.http_url_to_repo
-        let target = path </> project.path_with_namespace
-        clone url target
+        let url =
+            insertToken baseAddress token project.Url
 
-    IO.Directory.CreateDirectory path |> ignore
+        let target = path </> project.Path
+        clone url target printMode
+
+    if printMode then
+        printfn "%s" "PrintMode: printonly"
+    else
+        IO.Directory.CreateDirectory path |> ignore
 
     buildUrl baseAddress group
     |> download token
@@ -47,14 +58,16 @@ type Cmd =
     | [<Mandatory; AltCommandLine("-g")>] Group of string
     | [<Mandatory; AltCommandLine("-o")>] Output of string
     | [<Mandatory; AltCommandLine("-t")>] Token of string
-    
+    | [<AltCommandLine("-p")>] PrintOnly
+
     interface Argu.IArgParserTemplate with
         member this.Usage =
             match this with
             | BaseAddress _ -> "specify your gitlab instance base address"
             | Group _ -> "specify the group name or id to clone recursively"
-            | Output _ -> "specify the output folder to clone to" 
+            | Output _ -> "specify the output folder to clone to"
             | Token _ -> "specify your access token"
+            | PrintOnly -> "print theorical path without actually cloning"
 
 let help = """Kamino                                GitLab Organisation Cloner
 ----------------------------------------------------------------
@@ -63,14 +76,27 @@ Usage: kamino -b my-gitlab.com -o C:\Development\Git\ -g 42 -t xT0K3Nx4CC355x
 
 [<EntryPoint>]
 let main argv =
-    let processName = 
-        let name = IO.Path.GetFileNameWithoutExtension (Diagnostics.Process.GetCurrentProcess().MainModule.FileName )
-        if String.Equals(name, "dotnet", StringComparison.OrdinalIgnoreCase) then
+    let equalsDotNet name =
+        String.Equals(name, "dotnet", StringComparison.OrdinalIgnoreCase)
+
+    let processName =
+        let isDotNet =
+            Diagnostics
+                .Process
+                .GetCurrentProcess()
+                .MainModule
+                .FileName
+            |> IO.Path.GetFileNameWithoutExtension
+            |> equalsDotNet
+
+        if isDotNet then
             "dotnet kamino"
         else
             "kamino"
 
-    let parser = ArgumentParser<Cmd>(programName = processName)
+    let parser =
+        ArgumentParser<Cmd>(programName = processName)
+
     try
         let cmd = parser.ParseCommandLine(argv)
 
@@ -78,11 +104,11 @@ let main argv =
         let group = cmd.GetResult Group
         let output = cmd.GetResult Output
         let token = cmd.GetResult Token
+        let printMode = cmd.Contains PrintOnly
 
-        cloneOrganisation baseAddress group output token
+        cloneOrganisation baseAddress group output token printMode
         0
-    with
-    | :? Argu.ArguParseException ->
+    with :? Argu.ArguParseException ->
         printfn $"%s{help}"
         printfn $"%s{parser.PrintUsage()}"
         1
